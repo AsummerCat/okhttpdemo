7c9eee5bc891c9976b7252f6d33e439a1b952821
chenxiaocehng <583188551@qq.com>
2019-11-08 12:02:03 +0800

    添加其他构建方式

--- src/main/java/OkHttpDemo.java
+++ src/main/java/OkHttpDemo.java
@@ -1,9 +1,7 @@
 import okhttp3.*;
 
 public class OkHttpDemo {
-	//https://square.github.io/okhttp/
 	public static void test() {
-		System.out.println("hello");
 		  final MediaType JSON = MediaType.get("application/json; charset=utf-8");
 			OkHttpClient client = new OkHttpClient();
 			//构造builder
@@ -20,6 +18,44 @@ public class OkHttpDemo {
 				System.out.println("返回结果end");
 
 			} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	public static void test1() {
+		final MediaType JSON = MediaType.get("application/json; charset=utf-8");
+		OkHttpClient client = new OkHttpClient();
+		Request build = new Request.Builder().url("http://www.baidu.com").header("Content-Type", "application/json").post(RequestBody.create(JSON, "json数据")).build();
+		//发送请求请求
+		try (Response response = client.newCall(build).execute()) {
+			System.out.println("返回结果start");
+			response.body().string();
+			System.out.println("返回结果end");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	public static void test2() {
+		final MediaType JSON = MediaType.get("application/json; charset=utf-8");
+		OkHttpClient client = new OkHttpClient();
+
+		RequestBody body = RequestBody.create(JSON, "json数据");
+		Request request = new Request.Builder()
+				.header("Content-Type", "application/json")
+				.url("http://www.baidu.com")
+				.post(body)
+				.build();
+		//发送请求请求
+		try (Response response = client.newCall(request).execute()) {
+			System.out.println("返回结果start");
+			response.body().string();
+			System.out.println("返回结果end");
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
